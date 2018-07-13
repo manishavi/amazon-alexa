0d98278a1c710cdbb9b79301357d2f34c58fcbac
manishavi <manishalal972@gmail.com>
2018-07-13 15:26:09 -0500

    fixing

--- _dev/getMediumPosts.js
+++
@@ -1,9 +0,0 @@
-const mediumJSONFeed = require('medium-json-feed');
-
-mediumJSONFeed('free-code-camp/latest')
-  .then(data => {
-    console.log(JSON.stringify(data))  
-  })
-  .catch(data => {
-    console.log(JSON.stringify(data))
-  });
